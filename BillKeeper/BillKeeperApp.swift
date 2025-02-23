7c428b36478beb1028068aae3945b797261a49c9
Maxime Hutinet <maxime@hutinet.fr>
2025-02-23 20:30:20 +0100

    Initial Commit

---
+++ BillKeeper/BillKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillKeeperApp.swift
+//  BillKeeper
+//
+//  Created by Maxipoo on 23/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BillKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
